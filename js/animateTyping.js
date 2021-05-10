0791492965c1a4d77279715e9f0f28af0fb82c0a
Trafford Fewster <trafford.fewster@netmatters-scs.com>
2021-05-10 08:19:57 +0100

    Replace arrow functions cos IE

--- js/animateTyping.js
+++ js/animateTyping.js
@@ -1,16 +1,16 @@
 
-$(".animateText").each( (i,e) => {
+$(".animateText").each( function(i,e) {
     let currentText = $(e).text();
     let count = 0;
     let speed = $(e).attr("data-typing-speed") || 100;
     let delay = $(e).attr("data-typing-delay") || 0;
 
     $(e).text("")
-    setTimeout(() => {
+    setTimeout(function() {
         while (count <= currentText.length){
             let thisRun = currentText.substr(0,count);
             let delay = count * speed;
-            setTimeout(() => {
+            setTimeout(function() {
                 $(e).text(thisRun);
             }, delay);
             count++
