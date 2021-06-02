7cf1afa1a8064341afa483b48887ba85e7cce306
Trafford Fewster <trafford.fewster@netmatters-scs.com>
2021-06-02 10:15:41 +0100

    Add SQL Example | Make code examples scrollable

--- dist/codeCards.js
+++ dist/codeCards.js
@@ -1,83 +1,83 @@
 "use strict";
 
-var projHolder = document.getElementById("projectHolder");
+var projHolder = document.getElementById('projectHolder');
 var projects = [{
-  title: "Netmatters Clients Hover",
+  title: 'Netmatters Clients Hover',
   desc: "One of the issues I had when creating the <a href='https://www.netmatters.co.uk/'>Netmatters</a> home page was the clients going from greyscale to coloured at first I tried to just use CSS filters but that wouldn't work as we needed it to work as <a href='https://caniuse.com/?search=filter'>Internet Explorer does not support filters</a> so I ended up having to use two different images and swap them in and out.",
-  img: "img/codeExamples/udido.png",
-  github: "https://github.com/TraffordFewster/HTML---CSS-Reflection/blob/master/scss/content/_client-footer.scss#L6",
-  link: "https://netmatters.trafford-fewster.netmatters-scs.co.uk"
+  img: 'img/codeExamples/udido.png',
+  github: 'https://github.com/TraffordFewster/HTML---CSS-Reflection/blob/master/scss/content/_client-footer.scss#L6',
+  link: 'https://netmatters.trafford-fewster.netmatters-scs.co.uk'
 }, {
-  title: "Placeholder",
-  desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, arcu vel tempor luctus, justo ante gravida turpis, sit amet scelerisque dolor erat id ipsum. Etiam a est vel elit semper consectetur non ac diam. Integer turpis sapien, congue sed quam ut, blandit porta risus. Suspendisse urna mi, faucibus at facilisis a, facilisis nec erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ex dui, finibus ultrices.",
-  img: "img/projPlaceholder.jpg",
-  github: "https://github.com",
-  link: "https://example.com"
+  title: 'SQL Example',
+  desc: 'As part of the <a href="scs.html">SCS Scheme</a> we was tasked with creating an SQL query that includes a subquery with formatted outputs and is sorted by a value. The table I was working with had the structure that can be viewed <a href="misc/sql_pre.html">here</a>. <br><br>So with this structure in mind I set out a goal of selecting all employees who were hired before their manager so I started by selecting what I thought would be some usefull infomation and also formatted the two seperated name columns into one, after selecting I needed to filter it down using a subquery which will output the employee_id if the managers hire_date it greater than the employees hire date.<br><br>After running the query through the <a href="https://www.w3resource.com/sql-exercises/employee-database-exercise/subqueries-exercises-on-employee-database.php#SQLEDITOR">w3resource</a> SQL exercice I got the results which can be viewed <a href="misc/sql_post.html">here</a>',
+  img: 'img/codeExamples/sql.png',
+  github: undefined,
+  link: 'https://www.w3resource.com/sql-exercises/employee-database-exercise/subqueries-exercises-on-employee-database.php#SQLEDITOR'
 }, {
-  title: "Placeholder",
-  desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, arcu vel tempor luctus, justo ante gravida turpis, sit amet scelerisque dolor erat id ipsum. Etiam a est vel elit semper consectetur non ac diam. Integer turpis sapien, congue sed quam ut, blandit porta risus. Suspendisse urna mi, faucibus at facilisis a, facilisis nec erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ex dui, finibus ultrices.",
-  img: "img/projPlaceholder.jpg",
-  github: "https://github.com",
-  link: "https://example.com"
+  title: 'Placeholder',
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, arcu vel tempor luctus, justo ante gravida turpis, sit amet scelerisque dolor erat id ipsum. Etiam a est vel elit semper consectetur non ac diam. Integer turpis sapien, congue sed quam ut, blandit porta risus. Suspendisse urna mi, faucibus at facilisis a, facilisis nec erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ex dui, finibus ultrices.',
+  img: 'img/projPlaceholder.jpg',
+  github: 'https://github.com',
+  link: 'https://example.com'
 }, {
-  title: "Placeholder",
-  desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, arcu vel tempor luctus, justo ante gravida turpis, sit amet scelerisque dolor erat id ipsum. Etiam a est vel elit semper consectetur non ac diam. Integer turpis sapien, congue sed quam ut, blandit porta risus. Suspendisse urna mi, faucibus at facilisis a, facilisis nec erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ex dui, finibus ultrices.",
-  img: "img/projPlaceholder.jpg",
+  title: 'Placeholder',
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, arcu vel tempor luctus, justo ante gravida turpis, sit amet scelerisque dolor erat id ipsum. Etiam a est vel elit semper consectetur non ac diam. Integer turpis sapien, congue sed quam ut, blandit porta risus. Suspendisse urna mi, faucibus at facilisis a, facilisis nec erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ex dui, finibus ultrices.',
+  img: 'img/projPlaceholder.jpg',
   github: undefined,
   link: undefined
 }, {
-  title: "Placeholder",
-  desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, arcu vel tempor luctus, justo ante gravida turpis, sit amet scelerisque dolor erat id ipsum. Etiam a est vel elit semper consectetur non ac diam. Integer turpis sapien, congue sed quam ut, blandit porta risus. Suspendisse urna mi, faucibus at facilisis a, facilisis nec erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ex dui, finibus ultrices.",
-  img: "img/projPlaceholder.jpg",
-  github: "https://github.com",
+  title: 'Placeholder',
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, arcu vel tempor luctus, justo ante gravida turpis, sit amet scelerisque dolor erat id ipsum. Etiam a est vel elit semper consectetur non ac diam. Integer turpis sapien, congue sed quam ut, blandit porta risus. Suspendisse urna mi, faucibus at facilisis a, facilisis nec erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ex dui, finibus ultrices.',
+  img: 'img/projPlaceholder.jpg',
+  github: 'https://github.com',
   link: undefined
 }, {
-  title: "Placeholder",
-  desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, arcu vel tempor luctus, justo ante gravida turpis, sit amet scelerisque dolor erat id ipsum. Etiam a est vel elit semper consectetur non ac diam. Integer turpis sapien, congue sed quam ut, blandit porta risus. Suspendisse urna mi, faucibus at facilisis a, facilisis nec erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ex dui, finibus ultrices.",
-  img: "img/projPlaceholder.jpg",
+  title: 'Placeholder',
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim, arcu vel tempor luctus, justo ante gravida turpis, sit amet scelerisque dolor erat id ipsum. Etiam a est vel elit semper consectetur non ac diam. Integer turpis sapien, congue sed quam ut, blandit porta risus. Suspendisse urna mi, faucibus at facilisis a, facilisis nec erat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam ex dui, finibus ultrices.',
+  img: 'img/projPlaceholder.jpg',
   github: undefined,
-  link: "https://example.com"
+  link: 'https://example.com'
 }];
-var htmlStr = "";
+var htmlStr = '';
 
 for (var data in projects) {
   var key = data;
   data = projects[data];
-  htmlStr += "<div class='projectCard codeCard'  onclick='projectPopup(" + key + ")'>" + "<div class='imgHolder'>" + "<img src='" + data.img + "' alt='" + data.title + " preview image'>" + "</div>" + "<div class='textHolder'>" + "<h1>" + data.title + "</h1>" + "<h4>View Project <i class='fas fa-arrow-right'></i></h4>" + "</div>" + "</div>"; // Thanks IE for not support multiline strings so fun :)
+  htmlStr += "<div class='projectCard codeCard'  onclick='projectPopup(" + key + ")'>" + "<div class='imgHolder'>" + "<img src='" + data.img + "' alt='" + data.title + " preview image'>" + '</div>' + "<div class='textHolder'>" + '<h1>' + data.title + '</h1>' + "<h4>View Project <i class='fas fa-arrow-right'></i></h4>" + '</div>' + '</div>'; // Thanks IE for not support multiline strings so fun :)
 }
 
-var projH = document.getElementById("projectPopupHolder");
-var projImage = document.getElementById("projPopupImg");
-var projTitle = document.getElementById("projPopupTitle");
-var projDesc = document.getElementById("projPopupDesc");
-var projGithub = document.getElementById("projPopupGithubIcon");
-var projLink = document.getElementById("projPopupLinkIcon");
-var projClose = document.getElementById("projPopupClose");
+var projH = document.getElementById('projectPopupHolder');
+var projImage = document.getElementById('projPopupImg');
+var projTitle = document.getElementById('projPopupTitle');
+var projDesc = document.getElementById('projPopupDesc');
+var projGithub = document.getElementById('projPopupGithubIcon');
+var projLink = document.getElementById('projPopupLinkIcon');
+var projClose = document.getElementById('projPopupClose');
 
 function projectPopup(dataKey) {
   var data = projects[dataKey];
-  projH.classList.add("showPopup");
+  projH.classList.add('showPopup');
   projImage.src = data.img;
   projTitle.innerText = data.title;
   projDesc.innerHTML = data.desc;
 
   if (data.github !== undefined) {
-    projGithub.classList.remove("projDNone");
+    projGithub.classList.remove('projDNone');
     projGithub.href = data.github;
   } else {
-    projGithub.classList.add("projDNone");
+    projGithub.classList.add('projDNone');
   }
 
   if (data.link !== undefined) {
-    projLink.classList.remove("projDNone");
+    projLink.classList.remove('projDNone');
     projLink.href = data.link;
   } else {
-    projLink.classList.add("projDNone");
+    projLink.classList.add('projDNone');
   }
 }
 
 projClose.onclick = function () {
-  projH.classList.remove("showPopup");
+  projH.classList.remove('showPopup');
 };
 
 projHolder.innerHTML = htmlStr;
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,45 @@
-const gulp = require('gulp')
+const gulp = require('gulp');
 const sass = require('gulp-sass');
 const uglifycss = require('gulp-uglifycss');
-const babel = require('gulp-babel')
- 
+const babel = require('gulp-babel');
+
 sass.compiler = require('node-sass');
- 
+
 gulp.task('sass', function () {
-  return gulp.src('./scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+	return gulp
+		.src('./scss/style.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
 });
- 
+
 gulp.task('css', function () {
-  gulp.src('./css/*.css')
-    .pipe(uglifycss({
-      "uglyComments": true
-    }))
-    .pipe(gulp.dest('./dist/'));
+	return gulp
+		.src('./css/*.css')
+		.pipe(
+			uglifycss({
+				uglyComments: true,
+			})
+		)
+		.pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('es6', () => {
-  return gulp.src("js/*.js")
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(gulp.dest('dist'));
-})
-
-gulp.task('run', gulp.parallel("sass", "css", "es6"));
-
-gulp.task('watch', function() {
-    gulp.watch('./scss/*.scss', gulp.parallel('sass'));
-    gulp.watch('./css/*.css', gulp.parallel('css'));
-    gulp.watch("js/*.js", gulp.parallel('es6'));
+	return gulp
+		.src('js/*.js')
+		.pipe(
+			babel({
+				presets: ['@babel/env'],
+			})
+		)
+		.pipe(gulp.dest('dist'));
 });
 
+gulp.task('run', gulp.parallel('sass', 'css', 'es6'));
 
-gulp.task('default', gulp.parallel("run", "watch"));
+gulp.task('watch', function () {
+	gulp.watch('./scss/*.scss', gulp.parallel('sass'));
+	gulp.watch('./css/*.css', gulp.parallel('css'));
+	gulp.watch('js/*.js', gulp.parallel('es6'));
+});
+
+gulp.task('default', gulp.parallel('run', 'watch'));
